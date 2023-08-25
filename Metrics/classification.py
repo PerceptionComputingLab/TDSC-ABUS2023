a1dcb543692cc01f4b2be189cade5b4474935adb
xumingw <matthianxu@gmail.com>
2023-08-25 11:15:23 +0800

    Fix classification metric

--- Metrics/classification.py
+++ Metrics/classification.py
@@ -1,21 +1,21 @@
-from sklearn.metrics import accuracy_score,roc_auc_score
+from sklearn.metrics import accuracy_score, roc_auc_score
 
 import pandas as pd
+
 
 def score_aggregates(gt_csv_path, pred_csv_path):
     gt_info = pd.read_csv(gt_csv_path)
     gt_info = gt_info["class"]
     pred_info = pd.read_csv(pred_csv_path)
-    pred_info = pred_info["class"]
+    pred_info = pred_info["prob"]
     label_map = {'M': 1, 'B': 0}
     y_true = [label_map[label] for label in gt_info]
     y_pred = pred_info
     return {
-        "accuracy": accuracy_score(y_true,y_pred),
-        "auc": roc_auc_score(y_true,y_pred),
-        "score": (roc_auc_score(y_true,y_pred)+accuracy_score(y_true,y_pred))/2
+        "accuracy": accuracy_score(y_true, y_pred),
+        "auc": roc_auc_score(y_true, y_pred),
+        "score": (roc_auc_score(y_true, y_pred) + accuracy_score(y_true, y_pred)) / 2
     }
-
 
 
 if __name__ == "__main__":
